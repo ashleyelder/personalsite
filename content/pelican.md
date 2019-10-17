60f96e9f7fc584cf352532c1dd0abd60901a4eba
ashleyelder <shlee.elder@gmail.com>
2019-10-16 21:45:36 -0600

    renders author bio

--- content/pelican.md
+++ content/pelican.md
@@ -1,7 +1,7 @@
-Title: First Post
-Slug: first-post
+Title: Setting up Pelican
+Slug: pelican-portfolio-website
 Date: 2019-10-16 19:00
 
-##First blog post
+This is my first sample post, created with Pelican. I chose to re-create my portfolio site today with Pelican so that styling is taken care of by a theme and I can leverage this site for posts that are more case-study-type articles about projects I've completed or more fun things like that.
 
-First post done with pelican.
+Thanks for checking it out.
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,22 +2,16 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Ashley Elder'
-SITENAME = 'Ashley Elder'
+AUTHOR = u'Ashley'
+SITENAME = u'Ashley Elder'
+SITESUBTITLE = u'welcome to my personal website'
 SITEURL = ''
 
-AUTHORS_BIO = {
-  "ashley": {
-    "name": "Ashley",
-    "cover": "https://raw.githubusercontent.com/ashleyelder/fileHosting/master/eldorado.png",
-    "image": "https://arulrajnet.github.io/attila-demo/assets/images/avatar.png",
-    "website": "http://ashleyelder.net",
-    "location": "Boulder, CO, USA",
-    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
-  }
-}
+HEADER_COVER = 'https://raw.githubusercontent.com/ashleyelder/fileHosting/master/eldorado.png'
 
 PATH = 'content'
+
+DEFAULT_DATE_FORMAT = '%d %b %Y'
 
 TIMEZONE = 'America/Denver'
 
@@ -31,18 +25,39 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/ashleyelder'),
-          ('linkedin','https://www.linkedin.com/in/ashleyelder/'))
+          ('linkedin','https://www.linkedin.com/in/ashleyelder'))
 
 DEFAULT_PAGINATION = 5
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
 THEME = 'attila-1.3'
+
+AUTHORS_BIO = {
+  "ashley": {
+    "name": "Ashley",
+    # "cover": "https://raw.githubusercontent.com/ashleyelder/fileHosting/master/eldorado.png",
+    "image": "/images/avatar.png",
+    "website": "http://ashleyelder.net",
+    "linkedin": "ashleyelder",
+    "github": "ashleyelder",
+    "location": "Boulder, CO, USA",
+    "bio": "Full stack developer particularly interested in developing software that scales with best practices. I enjoy building things."
+  }
+}
